e40eea314e95ae0f893ab3faa22d17a948b72758
HenryEcker <HenryEcker@users.noreply.github.com>
2023-04-06 13:35:22 -0400

    Separate meta file

---
+++ dist/meta/BanEvasionAccountDeleteHelper.meta.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Ban Evasion Account Delete Helper
+// @description  Adds streamlined interface for deleting evasion accounts, then annotating and messaging the main accounts
+// @homepage     https://github.com/HenryEcker/SO-Mod-UserScripts
+// @author       Henry Ecker (https://github.com/HenryEcker)
+// @version      0.3.5
+// @downloadURL  https://github.com/HenryEcker/SO-Mod-BanEvasionAccountDeleteHelper/raw/master/dist/BanEvasionAccountDeleteHelper.user.js
+// @updateURL    https://github.com/HenryEcker/SO-Mod-BanEvasionAccountDeleteHelper/raw/master/dist/meta/BanEvasionAccountDeleteHelper.meta.js
+//
+// @match        *://*askubuntu.com/users/account-info/*
+// @match        *://*mathoverflow.net/users/account-info/*
+// @match        *://*serverfault.com/users/account-info/*
+// @match        *://*stackapps.com/users/account-info/*
+// @match        *://*stackexchange.com/users/account-info/*
+// @match        *://*stackoverflow.com/users/account-info/*
+// @match        *://*superuser.com/users/account-info/*
+//
+// @grant        none
+//
+// ==/UserScript==
+/* globals StackExchange, Stacks, $ */
